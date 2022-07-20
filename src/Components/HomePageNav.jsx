d472344bf8241489f478860c7432b57b80e273bc
surajDongre-16 <surajdongre8@gmail.com>
2022-07-20 19:31:51 +0530

    homepage navbar completed

---
+++ src/Components/HomePageNav.jsx
@@ -0,0 +1,89 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Image,
+  Input,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+
+const cardData = [
+  {
+    logo: "https://res.cloudinary.com/urbanclap/image/upload/q_auto,f_auto,fl_progressive:steep,w_64/t_high_res_template/categories/category_v2/category_1312fb60.png",
+    service: "Women's Therapies",
+    desc: "Women's...",
+  },
+  {
+    logo: "https://res.cloudinary.com/urbanclap/image/upload/q_auto,f_auto,fl_progressive:steep,w_64/t_high_res_template/images/growth/home-screen/1609757629780-2b2187.png",
+    service: "Salon For Men",
+    desc: "Salon For Men",
+  },
+  {
+    logo: "https://res.cloudinary.com/urbanclap/image/upload/q_auto,f_auto,fl_progressive:steep,w_64/t_high_res_template/images/growth/home-screen/1609757731250-ba3308.png",
+    service: "Men's Therapies",
+    desc: "Men's Therapies",
+  },
+];
+
+const HomePageNav = () => {
+  return (
+    <Box
+      boxShadow="rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px"
+      h="70px"
+      display={"flex"}
+      justifyContent="space-around"
+    >
+      <Container
+        maxW="4xl"
+        ml="10%"
+        p="10px"
+        display="flex"
+        justifyContent={"space-between"}
+      >
+        <Button
+          borderRadius={"5px 0 0 5px"}
+          bg="white"
+          h="100%"
+          border=" 1px solid #ececec"
+          borderRight="none"
+          colorScheme={"white"}
+          color="black"
+        >
+          <AiOutlineSearch fontSize={"25px"} />
+        </Button>
+        <Input
+          placeholder="Search for services"
+          w="100%"
+          h="100%"
+          borderLeft="none"
+          focusBorderColor="none"
+        />
+      </Container>
+      <Flex mr="10%">
+        {cardData.map((item) => (
+          <Tooltip hasArrow label={item.service} fontSize="10px">
+            <Button
+              bg="white"
+              borderRadius={"none"}
+              display="flex"
+              flexDirection={"column"}
+              justifyContent="space-around"
+              alignItems={"center"}
+              h="100%"
+              w="100px"
+            >
+              <Image src={item.logo} alt={item.service} h="30px" />
+              <Text fontSize={"10px"}>{item.desc}</Text>
+            </Button>
+          </Tooltip>
+        ))}
+      </Flex>
+    </Box>
+  );
+};
+
+export default HomePageNav;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
+import HomePageNav from "../Components/HomePageNav";
 import HomeTopSection from "../Components/HomeTopSection";
 
 const Home = () => {
   return <Box>
-    <HomeTopSection/>
+    {/* <HomeTopSection/> */}
+    <HomePageNav/>
   </Box>
 };
 
