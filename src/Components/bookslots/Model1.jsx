19198fe7a72dc07f59364ff47ec7b51928014187
surajDongre-16 <surajdongre8@gmail.com>
2022-07-22 23:54:45 +0530

    slot booking completed

--- src/Components/bookslots/Model1.jsx
+++ src/Components/bookslots/Model1.jsx
@@ -14,6 +14,7 @@ import {
   SimpleGrid,
   Text,
   useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import React from "react";
 import { FaHome, FaAngleRight } from "react-icons/fa";
@@ -23,29 +24,48 @@ import { useState } from "react";
 import styles from "../../StyleComponents/Payment.module.css";
 import { loadData, saveData } from "../../Utils/localstorage";
 
-const dateBox = {
-  border: "1px solid #ececec",
-  borderRadius: "10px",
-  h: "65px",
-  w: "65px",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-around",
-  alignItems: "center",
-  cursor: "pointer",
-};
-
+const schedule = [
+  "10:00 AM",
+  "11:00 AM",
+  "12:00 PM",
+  "01:00 PM",
+  "02:00 PM",
+  "03:00 PM",
+  "04:00 PM",
+  "05:00 PM",
+  "06:00 PM",
+];
 const days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
-
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let localday = saveData("bookDay", "");
+let localtime = saveData("time", "");
+let localdate = saveData("bookDate");
+const date = new Date();
+const month = date.getMonth();
+const currentDay = date.getDay();
+let curMonth = "";
+months.map((el, i) => {
+  if (i === month) curMonth = el;
+});
 const Model1 = () => {
-  const date = new Date();
-  const currentDay = date.getDay();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [modelOpen, setModelOpen] = useState(false);
-  const [trig1, setTrig1] = useState(false);
-  const [trig2, setTrig2] = useState(false);
-  const [trig3, setTrig3] = useState(false);
-
+  const toast = useToast();
+  const [trig, setTrig] = useState(-1);
+  const [t, setT] = useState(-1);
   const modelClose = () => {
     setModelOpen(false);
   };
@@ -54,19 +74,45 @@ const Model1 = () => {
   };
 
   const addDate = (date, day) => {
-    saveData("bookDate", { date, day });
+    setTrig(date);
+    saveData("bookDate", date);
+    saveData("bookDay", day);
   };
-  const addTime = (time) => {
+  const addTime = (time, index) => {
     saveData("time", time);
+    setT(index);
   };
+
   const payData = () => {
-    let d = loadData("bookDate");
-    let t = loadData("time");
-    console.log(d, t);
+    localday = loadData("bookDay");
+    localdate = loadData("bookDate");
+    localtime = loadData("time");
+    if (localday && localtime && localdate) {
+      onClose();
+      modelOp();
+    } else {
+      toast({
+        title: "Booking Failed",
+        description: "Select both Date and Time",
+        status: "error",
+        duration: 1000,
+        isClosable: true,
+      });
+    }
   };
   return (
     <>
-      <Button onClick={onOpen}>Trigger modal</Button>
+      <Button
+        onClick={() => {
+          setTrig(-1);
+          setT(-1);
+          saveData("bookDate", "");
+          saveData("time", "");
+          onOpen();
+        }}
+      >
+        Trigger modal
+      </Button>
 
       <Modal onClose={onClose} size={"3xl"} isOpen={isOpen} isCentered>
         <ModalOverlay />
@@ -90,26 +136,21 @@ const Model1 = () => {
                 ind + 1 === currentDay ? (
                   <>
                     <Box
-                      {...dateBox}
-                      className={trig1 ? styles.selected : ""}
+                      className={`${styles.dateBox} ${
+                        trig === date.getDate() ? styles.selected : ""
+                      }`}
                       onClick={() => {
                         addDate(date.getDate(), el);
-                        setTrig1(true);
-                        setTrig2(false);
-                        setTrig3(false);
                       }}
                     >
                       <Text>{el}</Text>
                       <Text fontWeight={"bolder"}>{date.getDate()}</Text>
                     </Box>
                     <Box
-                      {...dateBox}
-                      className={trig2 ? styles.selected : ""}
+                      className={`${styles.dateBox}
+                        ${trig === date.getDate() + 1 ? styles.selected : ""}`}
                       onClick={() => {
                         addDate(date.getDate() + 1, days[ind + 1]);
-                        setTrig1(false);
-                        setTrig2(true);
-                        setTrig3(false);
                       }}
                     >
                       <Text>
@@ -118,13 +159,10 @@ const Model1 = () => {
                       <Text fontWeight={"bolder"}>{date.getDate() + 1}</Text>
                     </Box>
                     <Box
-                      {...dateBox}
-                      className={trig3 ? styles.selected : ""}
+                      className={`${styles.dateBox}
+                        ${trig === date.getDate() + 2 ? styles.selected : ""}`}
                       onClick={() => {
                         addDate(date.getDate() + 2, days[ind + 2]);
-                        setTrig1(false);
-                        setTrig2(false);
-                        setTrig3(true);
                       }}
                     >
                       <Text>
@@ -140,16 +178,7 @@ const Model1 = () => {
             </Flex>
             <VscTriangleUp />
             <Box w="100%">
-              <Flex
-                color="gray"
-                h="40px"
-                // mt="-2%"
-                bg="#fbf6f7 "
-                justifyContent={"left"}
-                alignItems="center"
-                fontSize={"16px"}
-                fontWeight="300"
-              >
+              <Flex className={styles.note}>
                 <AiOutlineCreditCard fontSize={"20px"} />
                 <Text mt="2%">
                   Free cancelation till 2 hrs before the booked slot, post that
@@ -162,26 +191,13 @@ const Model1 = () => {
                 Time to start the service
               </Heading>
               <SimpleGrid columns={3} rows={3} spacing={10} mt="5%">
-                {[
-                  "10:00 AM",
-                  "11:00 AM",
-                  "12:00 PM",
-                  "01:00 PM",
-                  "02:00 PM",
-                  "03:00 PM",
-                  "04:00 PM",
-                  "05:00 PM",
-                  "06:00 PM",
-                ].map((time, index) => (
+                {schedule.map((time, index) => (
                   <Box
                     key={index}
-                    border="1px solid #ececec "
-                    h="50px"
-                    borderRadius="10px"
-                    textAlign={"center"}
-                    pt="5%"
-                    cursor="pointer"
-                    onClick={() => addTime(time)}
+                    onClick={() => addTime(time, index)}
+                    className={`${styles.scheduleBox} ${
+                      index === t ? `${styles.selected}` : ""
+                    }`}
                   >
                     {time}
                   </Box>
@@ -190,28 +206,36 @@ const Model1 = () => {
             </Box>
           </ModalBody>
           <ModalFooter>
-            <Button
-              onClick={() => {
-                payData();
-                onClose();
-                modelOp();
-              }}
-              w="100%"
-              colorScheme={"purple"}
-            >
+            <Button onClick={() => payData()} w="100%" colorScheme={"purple"}>
               Make Payment
             </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
-      {/* <Button onClick={modelOp}>SECOND</Button> */}
+
+      {/* Payment done model */}
 
       <Modal onClose={modelClose} isOpen={modelOpen} isCentered>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader
+            fontWeight={"700"}
+            color="purple.500"
+            textAlign={"center"}
+          >
+            Payment Successfull
+          </ModalHeader>
           <ModalCloseButton />
-          {/* <ModalBody><Lorem count={2} /></ModalBody> */}
+          <ModalBody>
+            <Heading as="h5" size="15px" mb="2%">
+              Your Booking Details
+            </Heading>
+            <Text fontWeight={"500"}>
+              Date of Booking : {`${localdate} ${curMonth} `}
+            </Text>
+            <Text fontWeight={"500"}>Day : {localday}</Text>
+            <Text fontWeight={"500"}>Time : {localtime}</Text>
+          </ModalBody>
           <ModalFooter>
             <Button onClick={modelClose}>Close</Button>
           </ModalFooter>
